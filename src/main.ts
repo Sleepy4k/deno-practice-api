cff789ef35c06bd985a8b431be7c1708c16630a2
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-10-20 22:45:53 +0700

    chore(main): add fallback route

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
-import router from './routes.ts';
-import { Application } from 'https://deno.land/x/oak@v12.6.0/mod.ts';
+import router from './routes/_index.ts';
+import { Status, Application } from 'https://deno.land/x/oak@v12.6.0/mod.ts';
 
 const app = new Application();
 const port = 8000;
 
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+app.use((ctx) => {
+  ctx.response.status = Status.NotFound;
+  ctx.response.headers.set('Content-Type', 'application/json');
+  ctx.response.body = JSON.stringify({
+    success: false,
+    message: 'Route Not Found',
+    data: []
+  });
+});
 
 console.log(`Server running on port ${port}`);
 
