294e8466356cfb41281f705729c1badc08e20952
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-10-21 13:09:14 +0700

    chore(model): remove unused code

--- src/models/_index.ts
+++ src/models/_index.ts
@@ -1,8 +1,5 @@
-import { Credentials } from '../configs/_index.ts';
-import { Client } from 'https://deno.land/x/postgres@v0.17.0/mod.ts';
-
-const Database = new Client(Credentials);
+import * as userModel from './user.model.ts';
 
 export {
-  Database
+  userModel
 };
