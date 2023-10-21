cd2e8e66772db2fed91810a8e3de83eeb436f94f
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-10-21 13:08:40 +0700

    chore(repository): change method to handle data

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,19 +1,14 @@
+import { userModel } from '../models/_index.ts';
 import type { UserType, FullUserType } from '../types/_index.ts';
 import { Request } from 'https://deno.land/x/oak@v12.6.0/mod.ts';
-import { Client } from 'https://deno.land/x/postgres@v0.17.0/mod.ts';
 
 /**
  * @desc Handle query for get users
  * 
- * @param Database Client
- * 
  * @returns QueryObjectResult<UserType>
  */
-const index = async ({ Database }: { Database: Client }) => {
-  await Database.connect();
-  const data = await Database.queryObject<UserType>('select id, email, role from users');
-  await Database.end();
-
+const index = async () => {
+  const data = await userModel.get<UserType>({ column: 'id, email, role' });
   return data;
 };
 
@@ -21,33 +16,20 @@ const index = async ({ Database }: { Database: Client }) => {
  * @desc Handle query for create user
  * 
  * @param request Request
- * @param Database Client
  * 
  * @returns QueryObjectResult<UserType> | string
  */
-const create = async ({ request, Database }: { request: Request, Database: Client }) => {
+const create = async ({ request }: { request: Request }) => {
   const body = request.body();
   const { email, password, password_confirmation } = await body.value;
 
-  if (!email || !password || !password_confirmation) {
-    return "Email, password, and password confirmation are required";
-  }
+  if (!email || !password || !password_confirmation) return "Email, password, and password confirmation are required";
+  if (password !== password_confirmation) return "Password confirmation not match";
 
-  if (password !== password_confirmation) {
-    return "Password confirmation not match";
-  }
+  const validate = await userModel.get<UserType>({ column: 'id, email, role', where: `email = '${email}'` });
+  if (validate.rowCount !== 0) return "Email already exists";
 
-  await Database.connect();
-  const validate = await Database.queryObject<UserType>(`select id, email, role from users where email = '${email}'`);
-
-  if (validate.rowCount !== 0) {
-    await Database.end();
-    return "Email already exists";
-  }
-
-  const data = await Database.queryObject<UserType>(`insert into users (email, role, password) values ('${email}', 'user', '${password}') returning id, email, role`);
-  await Database.end();
-  
+  const data = await userModel.create<UserType>({ column: 'email, role, password', value: `'${email}', 'user', '${password}'`, returning: 'id, email, role' });
   return data;
 };
 
@@ -55,19 +37,12 @@ const create = async ({ request, Database }: { request: Request, Database: Clien
  * @desc Handle query for get single user
  * 
  * @param id string
- * @param Database Client
  * 
  * @returns QueryObjectResult<UserType> | string
  */
-const find = async ({ id, Database }: { id: string, Database: Client }) => {
-  await Database.connect();
-
-  const data = await Database.queryObject<UserType>(`select id, email, role from users where id = ${id}`);
-  await Database.end();
-  
-  if (data.rowCount === 0) {
-    return "User not found";
-  }
+const find = async ({ id }: { id: string }) => {
+  const data = await userModel.get<UserType>({ column: 'id, email, role', where: `id = ${id}` });
+  if (data.rowCount === 0) return "User not found";
 
   return data;
 };
@@ -77,36 +52,21 @@ const find = async ({ id, Database }: { id: string, Database: Client }) => {
  * 
  * @param request Request
  * @param id string
- * @param Database Client
  * 
  * @returns QueryObjectResult<UserType> | string
  */
-const update = async ({ request, id, Database }: { request: Request, id: string, Database: Client }) => {
+const update = async ({ request, id }: { request: Request, id: string }) => {
   const body = request.body();
   const { email, password, password_confirmation } = await body.value;
 
-  if (!email || !password || !password_confirmation) {
-    return "Email, password, and password confirmation are required";
-  }
+  if (!email || !password || !password_confirmation) return "Email, password, and password confirmation are required";
+  if (password !== password_confirmation) return "Password confirmation not match";
 
-  if (password !== password_confirmation) {
-    return "Password confirmation not match";
-  }
+  const validate = await userModel.get<FullUserType>({ column: '*', where: `email = '${email}'` });
+  if (validate.rowCount !== 0) return "Email already exists";
 
-  await Database.connect();
-  const validate = await Database.queryObject<FullUserType>(`select id, email, role, password from users where email = '${email}'`);
-
-  if (validate.rowCount !== 0) {
-    await Database.end();
-    return "Email already exists";
-  }
-
-  const data = await Database.queryObject<UserType>(`update users set email = '${email}', password = '${password}' where id = ${id} returning id, email, role`);
-  await Database.end();
-
-  if (data.rowCount === 0) {
-    return "User not found";
-  }
+  const data = await userModel.update<UserType>({ column: 'email, password', value: `'${email}', '${password}'`, where: `id = ${id}`, returning: 'id, email, role' });
+  if (data.rowCount === 0) return "User not found";
 
   return data;
 };
@@ -115,19 +75,12 @@ const update = async ({ request, id, Database }: { request: Request, id: string,
  * @desc Handle query for delete single user
  * 
  * @param id string
- * @param Database Client
  * 
  * @returns QueryObjectResult<UserType> | string
  */
-const destroy = async ({ id, Database }: { id: string, Database: Client }) => {
-  await Database.connect();
-
-  const data = await Database.queryObject<UserType>(`delete from users where id = ${id} returning id, email, role`);
-  await Database.end();
-  
-  if (data.rowCount === 0) {
-    return "User not found";
-  }
+const destroy = async ({ id }: { id: string }) => {
+  const data = await userModel.destroy<UserType>({ where: `id = ${id}`, returning: 'id, email, role' });
+  if (data.rowCount === 0) return "User not found";
 
   return data;
 };
