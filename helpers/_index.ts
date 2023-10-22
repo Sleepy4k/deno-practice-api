dffe4f82a84144297ab31540a90bf7c3f2f8be8e
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-10-22 14:38:13 +0700

    chore(helper): change method to hash password

--- helpers/_index.ts
+++ helpers/_index.ts
@@ -1,7 +1,10 @@
 import { debug } from './debug.ts';
 import { response_json } from './response.ts';
+import { hash_password, verify_password } from './hashing.ts';
 
 export {
   debug,
-  response_json
+  response_json,
+  hash_password,
+  verify_password
 };
--- helpers/hashing.ts
+++ helpers/hashing.ts
@@ -1,16 +1,37 @@
-import { hash, verify } from 'https://raw.githubusercontent.com/Sleepy4k/deno-argon2-updated/master/lib/mod.ts';
+import "https://deno.land/x/dotenv@v3.2.2/load.ts";
+import { ArgonWorker, variant } from "https://deno.land/x/argon2ian@2.0.1/src/async.ts";
 
+const wrk = new ArgonWorker();
+const enco = new TextEncoder();
+const salt = Deno.env.get("PASS_SALT") || "SOME_SALT";
+
+/**
+ * @desc Hash password
+ * 
+ * @param password string
+ * 
+ * @returns string
+ */
 const hash_password = async (password: string) => {
-  const salt = crypto.getRandomValues(new Uint8Array(20));
-  const hashed_password = await hash(password, { salt });
+  const hashed_password = await wrk.hash(enco.encode(password), enco.encode(salt),
+    { t: 2, variant: variant.Argon2i }); // -> Uint8Array
 
-  return hashed_password;
+  return hashed_password.toString();
 }
 
+/**
+ * @desc Verify password
+ * 
+ * @param password string
+ * @param hashed_password string
+ * 
+ * @returns boolean
+ */
 const verify_password = async (password: string, hashed_password: string) => {
-  const verified_password = await verify(hashed_password, password);
+  const new_password = await hash_password(password);
+  if (new_password !== hashed_password) return false;
 
-  return verified_password;
+  return true;
 }
 
 export {
