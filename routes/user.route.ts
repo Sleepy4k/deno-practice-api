0d02f7dbe95e928e88799a4344c5eafab7530450
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-10-22 14:36:50 +0700

    chore(route): change user controller import

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,12 +1,12 @@
-import { user } from './../controllers/_index.ts';
+import { UserController } from './../controllers/_index.ts';
 import { Router } from 'https://deno.land/x/oak@v12.6.0/mod.ts';
 
 const router = new Router();
 
-router.get('/user', user.index);
-router.post('/user', user.create);
-router.get('/user/:id', user.find);
-router.put('/user/:id', user.update);
-router.delete('/user/:id', user.destroy);
+router.get('/user', UserController.index);
+router.post('/user', UserController.create);
+router.get('/user/:id', UserController.find);
+router.patch('/user/:id', UserController.update);
+router.delete('/user/:id', UserController.destroy);
 
 export default router;
